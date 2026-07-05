ed32736e504c8e9006e96268266963da204b662e
Jeffreyjacob <107020163+Jeffreyjacob@users.noreply.github.com>
2026-07-05 21:46:05 +0100

    working on docker setup, loki and grafana for monitoring

---
+++ dockerfile
@@ -0,0 +1,27 @@
+# --- Stage 1: builder
+
+FROM node:20-alpine AS builder
+WORKDIR /app
+RUN apk add --no-cache python3 make g++ openssl
+COPY package.json package-lock.json ./
+RUN npm ci  
+COPY . .
+RUN npx prisma generate
+RUN npm run build
+
+
+# --- stage 2: runner
+FROM node:20-alpine AS runner
+WORKDIR /app
+ENV NODE_ENV=production
+RUN apk add --no-cache openssl
+COPY package.json package-lock.json ./
+RUN npm ci --omit=dev
+COPY --from=builder /app/dist ./dist
+COPY  --from=builder /app/node_modules/.prisma ./node_modules/.prisma
+COPY --from=builder /app/node_modules/@prisma ./node_modules/@prisma
+COPY --from=builder /app/prisma ./prisma
+
+USER node
+EXPOSE 3000
+CMD [ "node","dist/server.js" ]
--- src/config/logger.ts
+++ src/config/logger.ts
@@ -2,29 +2,70 @@ import pino from "pino";
 import { env } from "./env";
 
 const isDev = env.NODE_ENV === "development";
+const hasLoki = !!process.env.LOKI_HOST;
 
-export const logger = pino({
-  level: isDev ? "debug" : "info",
-  ...(isDev && {
-    transport: {
+const getTransport = () => {
+  if (hasLoki) {
+    return pino.transport({
+      targets: [
+        {
+          target: "pino-loki",
+          level: "info",
+          options: {
+            host: process.env.LOKI_HOST,
+            label: {
+              app: "dev_desk",
+              env: env.NODE_ENV,
+            },
+            replaceTimestamp: true,
+            silenceErrors: false,
+          },
+        },
+        ...(isDev
+          ? [
+              {
+                target: "pino-pretty",
+                options: {
+                  colorize: true,
+                  translateTime: "SYS:standard",
+                  ignore: "pid,hostname",
+                },
+              },
+            ]
+          : []),
+      ],
+    });
+  }
+
+  if (isDev) {
+    return pino.transport({
       target: "pino-pretty",
       options: {
         colorize: true,
         translateTime: "SYS:standard",
         ignore: "pid,hostname",
       },
+    });
+  }
+
+  return undefined;
+};
+
+export const logger = pino(
+  {
+    level: isDev ? "debug" : "info",
+    redact: [
+      "*.password",
+      "*.token",
+      "req.headers.authorization",
+      "*.refreshtoken",
+      "*.resetToken",
+      "*.stripeCustomerId",
+    ],
+    base: {
+      name: "dev desk api",
+      env: env.NODE_ENV,
     },
-  }),
-  redact: [
-    "*.password",
-    "*.token",
-    "req.headers.authorization",
-    "*.refreshtoken",
-    "*.resetToken",
-    "*.stripeCustomerId",
-  ],
-  base: {
-    name: "dev desk api",
-    env: env.NODE_ENV,
   },
-});
+  getTransport()
+);
